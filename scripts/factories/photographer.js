b398d5d2e5dfe8ffcbd5989de9c499404e89b173
Rudy <Rudy.26.fr@gmail.com>
2023-01-25 13:50:06 +0100

    add 'createPhotographerHeader()'

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,47 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographerFactory(photographer) {
+
+    const { id, name, portrait, city, country, tagline } = photographer;
 
     const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+    function getPhotographerCardDOM() {
+        const article = document.createElement('article');
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        const a = document.createElement('a')
+        a.href = `photographer.html?id=${id}`
+        article.appendChild(a)
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        a.appendChild(img);
+        a.appendChild(h2);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    // Fonction pour créer l'entête de la page
+    function createPhotographerHeader() {
+        const div_name = document.createElement('div');
+        div_name.classList.add('photograph-header-name')
+        const h1_name = document.createElement('h1');
+        h1_name.innerText = name;
+        div_name.appendChild(h1_name);
+        const h2_name = document.createElement('h2');
+        h2_name.innerText = `${city}, ${country}`;
+        div_name.appendChild(h2_name);
+        const p_name = document.createElement('p');
+        p_name.innerText = tagline;
+        div_name.appendChild(p_name);
+
+
+        const div_img = document.createElement('div');
+        div_img.classList.add('photograph-header-picture')
+
+        const img = document.createElement('img');
+        img.setAttribute('src', picture)
+        div_img.appendChild(img);
+
+        return ({ div_name, div_img });
+    }
+
+    return { name, picture, getPhotographerCardDOM, createPhotographerHeader }
 }
