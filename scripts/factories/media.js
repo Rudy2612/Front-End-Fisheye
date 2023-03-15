2e144fb1aed9b62bf3b230fd6395e644e2670be7
Rudy <Rudy.26.fr@gmail.com>
2023-03-15 21:33:16 +0100

    media to medias

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,22 +1,22 @@
-export default function mediaFactory(media) {
+export default function mediaFactory() {
 
     //  ------- Création des vignette de media avec affichage titre et likes -------
-    function getMediaCardDOM(media) {
+    function getMediaCardDOM(medias) {
         const div = document.createElement('div');
         div.classList.add('media-container');
         
         // Pour chaque médias, créer une vignette
-        for (var i = 0; i < media.length; i++) {
+        for (var i = 0; i < medias.length; i++) {
             const divMedia = document.createElement('div');
             divMedia.classList.add('media-item');
-            if (media[i].image) {
+            if (medias[i].image) {
                 const img = document.createElement('img');
                 img.tabIndex = 10 + i;
                 img.classList.add('media-img');
                 img.setAttribute('data-media_position', i)
                 img.setAttribute('aria-controls', "modal")
-                img.setAttribute('src', `assets/images/${media[i].image.replaceAll(' ', '_')}`);
-                img.alt = media[i].image.replaceAll('_', " ").replaceAll('.jpg', "");
+                img.setAttribute('src', `assets/images/${medias[i].image.replaceAll(' ', '_')}`);
+                img.alt = medias[i].image.replaceAll('_', " ").replaceAll('.jpg', "");
                 divMedia.append(img);
             }
             else {
@@ -25,8 +25,8 @@ export default function mediaFactory(media) {
                 video.classList.add('media-img');
                 video.setAttribute('data-media_position', i)
                 video.setAttribute('aria-controls', "modal")
-                video.setAttribute('src', `assets/images/${media[i].video}`);
-                video.alt = media[i].video.replaceAll('_', " ").replaceAll('.mp4', "");
+                video.setAttribute('src', `assets/images/${medias[i].video}`);
+                video.alt = medias[i].video.replaceAll('_', " ").replaceAll('.mp4', "");
                 divMedia.append(video);
             }
             const mediaInfo = document.createElement('div');
@@ -34,20 +34,20 @@ export default function mediaFactory(media) {
 
             const mediaTitle = document.createElement('p');
             mediaTitle.classList.add('media-title');
-            mediaTitle.innerText = media[i].title;
+            mediaTitle.innerText = medias[i].title;
             mediaInfo.append(mediaTitle);
 
             const likeSection = document.createElement('div');
             const likeText = document.createElement('p');
             likeText.classList.add('media-likes');
-            likeText.id = "like-text-" + media[i].id;
-            likeText.innerText = media[i].likes;
+            likeText.id = "like-text-" + medias[i].id;
+            likeText.innerText = medias[i].likes;
             likeSection.append(likeText);
 
             const imgHeart = document.createElement('img')
             imgHeart.src = "../../assets/icons/heart.png"
             imgHeart.alt = "likes"
-            imgHeart.id = media[i].id;
+            imgHeart.id = medias[i].id;
             imgHeart.classList.add('media-heart');
             likeSection.append(imgHeart);
 
